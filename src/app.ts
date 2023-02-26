c5a361cb7e2bf32cc8ff9038e9ff4c382cb1742e
benzo-y <fukubenzo@gmail.com>
2023-02-26 12:13:15 +0900

    add 404 and error handler

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', usersRouter.router);
 
+app.all('*', (req, res) => {
+  res.send('404 Not Found');
+});
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).send('500 Internal Server Error');
+  }
+);
+
 export default app;
