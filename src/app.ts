1af49c941068e15603a567ee56f0e0bafc46ce92
benzo-y <fukubenzo@gmail.com>
2023-02-26 11:09:04 +0900

    sample post api

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import * as usersRouter from './routes/users';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/users', usersRouter.router);
 
 export default app;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,3 +11,22 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).send(allUsers);
   })();
 });
+
+router.post('/', (req: Request, res: Response) => {
+  (async () => {
+    const newUser = new db.users();
+    const tmpDate = new Date();
+
+    newUser.username = req.body.username;
+    newUser.email = req.body.email;
+    newUser.created_at = tmpDate;
+    newUser.updated_at = tmpDate;
+
+    try {
+      await db.users.create(newUser.dataValues);
+      res.send('ok');
+    } catch (error: unknown) {
+      res.status(400).send('ng');
+    }
+  })();
+});
