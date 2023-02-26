ef37851aaafcf73ee943c39b7849534cda60a86a
benzo-y <fukubenzo@gmail.com>
2023-02-26 11:08:44 +0900

    sample get api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as usersRouter from './routes/users';
 
 const app = express();
-app.use('/users', usersRouter.index);
+
+app.use('/users', usersRouter.router);
 
 export default app;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,24 +1,13 @@
 import { Request, Response } from 'express';
 import db from '../models';
+import express from 'express';
 
-/* GET users listing. */
-export const index = (req: Request, res: Response) => {
+export const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
   (async () => {
-    console.log('start');
-    const count = await db.users.count();
-    console.log(count);
-
-    const newUser = new db.users();
-    newUser.username = `hoge${count + 1}`;
-    newUser.email = `hoge${count + 1}@example.com`;
-    newUser.created_at = new Date();
-    newUser.updated_at = new Date();
-
-    await db.users.create(newUser.dataValues);
-
     const allUsers = await db.users.findAll();
-    console.log(allUsers);
-
-    res.send('respond with a resource');
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(200).send(allUsers);
   })();
-};
+});
